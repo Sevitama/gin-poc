ed73e763e1464727eacad8e9c4adc5e29dd5b4ce
Severin Grimm <severin.grimm@ost.ch>
2022-09-19 14:41:48 +0200

    add in-memory articles

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
+// main.go
+
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,25 +17,11 @@ func main() {
 	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
 
-	// Define the route for the index page and display the index.html template
-	// To start with, we'll use an inline route handler. Later on, we'll create
-	// standalone functions that will be used as route handlers.
-	router.GET("/", func(c *gin.Context) {
-
-		// Call the HTML method of the Context to render a template
-		c.HTML(
-			// Set the HTTP status to 200 (OK)
-			http.StatusOK,
-			// Use the index.html template
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-
-	})
+	// Handle Index
+	router.GET("/", showIndexPage)
+	router.GET("/article/view/:article_id", getArticle)
 
 	// Start serving the application
 	router.Run()
+
 }
---
+++ models.article.go
@@ -0,0 +1,29 @@
+package main
+
+import "errors"
+
+type article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+// Move article to DB later
+var articleList = []article{
+	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+}
+
+// Return a list of all the articles
+func getAllArticles() []article {
+	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("Article not found")
+}
