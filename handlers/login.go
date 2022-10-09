240ed6887db670f3f526710bb83585ee536f9d0b
Marco Martinez <mmmartinez@bluewin.ch>
2022-10-09 17:32:19 +0200

    use cooke

--- handlers/login.go
+++ handlers/login.go
@@ -1,32 +1,35 @@
 package handlers
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/Sevitama/gin-poc/models"
-  "github.com/gin-gonic/gin"
-  "github.com/gin-gonic/gin/binding"
+	"github.com/Sevitama/gin-poc/models"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 type SignInInput struct {
-  Username string `json:"username" binding:"required"`
-  Password string `json:"password" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 func SignIn(c *gin.Context) {
-  var input SignInInput
+	var input SignInInput
 
-  if err := c.ShouldBindWith(&input, binding.Form); err != nil {
-    c.HTML(http.StatusBadRequest,"error.html", gin.H{"error": err.Error()})
-    return
-  }
+	if err := c.ShouldBindWith(&input, binding.Form); err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "Invalid Input"})
+		return
+	}
 
-  token, err := models.LoginCheck(input.Username, input.Password)
+	token, err := models.LoginCheck(input.Username, input.Password)
 
-  if err != nil {
-    c.HTML(http.StatusBadRequest,"error.html", gin.H{"error": "username or password is incorrect."})
-    return
-  }
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "username or password is incorrect."})
+		return
+	}
 
-  c.HTML(http.StatusOK, "login.html", gin.H{"authenticated": "authenticated", "token": token})
+	c.SetCookie("jwt", token, 3600, "/", "localhost", true, true)
+	c.SetCookie("jwt_insecure", token, 3600, "/", "localhost", false, false)
+
+	c.HTML(http.StatusOK, "login.html", gin.H{"authenticated": "authenticated", "token": token})
 }
