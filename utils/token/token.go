38180be9d2fb5890b72c4605b5d52f09eddda6d0
Marco Martinez <mmmartinez@bluewin.ch>
2022-10-09 17:33:01 +0200

    use cookies

--- utils/token/token.go
+++ utils/token/token.go
@@ -1,68 +1,68 @@
 package token
 
 import (
-  "fmt"
-  "strconv"
-  "time"
+	"fmt"
+	"strconv"
+	"time"
 
-  "github.com/gin-gonic/gin"
-  jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 func TokenValid(c *gin.Context) error {
-  tokenString := ExtractToken(c)
-  _, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-    if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-      return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-    }
-    return []byte("secret"), nil
-  })
-  if err != nil {
-    return err
-  }
-  return nil
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func GenerateToken(user_id int) (string, error) {
 
-  token_lifespan,err := strconv.Atoi("1")
+	token_lifespan, err := strconv.Atoi("1")
 
-  if err != nil {
-    return "",err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  claims := jwt.MapClaims{}
-  claims["authorized"] = true
-  claims["user_id"] = user_id
-  claims["exp"] = time.Now().Add(time.Hour * time.Duration(token_lifespan)).Unix()
-  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = user_id
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(token_lifespan)).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-  return token.SignedString([]byte("secret"))
+	return token.SignedString([]byte("secret"))
 }
 
 func ExtractToken(c *gin.Context) string {
-  token := c.Query("token")
-  return token
+	token, _ := c.Cookie("jwt")
+	return token
 }
 
 func ExtractTokenID(c *gin.Context) (uint, error) {
-  tokenString := ExtractToken(c)
-  token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-    if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-      return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-    }
-    return []byte("secret"), nil
-  })
-  if err != nil {
-    return 0, err
-  }
-  claims, ok := token.Claims.(jwt.MapClaims)
-  if ok && token.Valid {
-    uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
-    if err != nil {
-      return 0, err
-    }
-    return uint(uid), nil
-  }
-  return 0, nil
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+	return 0, nil
 }
