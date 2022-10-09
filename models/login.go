1eaa9bd8e87d3ead8346c729db94d66eada462e4
Marco Martinez <mmmartinez@bluewin.ch>
2022-10-09 17:32:44 +0200

    use cookies

--- models/login.go
+++ models/login.go
@@ -3,52 +3,55 @@
 package models
 
 import (
-  "golang.org/x/crypto/bcrypt"
-  "github.com/Sevitama/gin-poc/utils/token"
+	"github.com/Sevitama/gin-poc/utils/token"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Credential struct {
-  Id           int    `json:"id"`
-  Username     string `json:"username"`
-  PasswordHash string `json:"passwordhash"`
+	Id           int    `json:"id"`
+	Username     string `json:"username"`
+	PasswordHash string `json:"passwordhash"`
 }
 
 type User struct {
-  Username string `json:"username"`
-  Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-func VerifyPassword(password,hashedPassword string) error {
-  return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+func VerifyPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
 
-func LoginCheck(username string, password string) (string,error) {
+func LoginCheck(username string, password string) (string, error) {
 
-  user := GetSingleCredential("SELECT * FROM accounts WHERE username = $1", []any{username})
+	user, invalid := GetSingleCredential("SELECT * FROM accounts WHERE username = $1", []any{username})
+	if invalid != nil {
+		return "", invalid
+	}
 
-  err := VerifyPassword(password, user.PasswordHash)
+	err := VerifyPassword(password, user.PasswordHash)
 
-  if err != nil || err == bcrypt.ErrMismatchedHashAndPassword {
-   return "", err
-  }
+	if err != nil || err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
 
-  token,err := token.GenerateToken(user.Id)
+	token, err := token.GenerateToken(user.Id)
 
-  if err != nil {
-   return "",err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  return token,nil
+	return token, nil
 }
 
-func GetSingleCredential(sqlStatement string, values []any) Credential {
-  credential := Credential{}
-  result := GetRecord(sqlStatement, values)
-  err := result.Scan(&credential.Id, &credential.Username, &credential.PasswordHash)
+func GetSingleCredential(sqlStatement string, values []any) (Credential, error) {
+	credential := Credential{}
+	result := GetRecord(sqlStatement, values)
+	err := result.Scan(&credential.Id, &credential.Username, &credential.PasswordHash)
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		return credential, err
+	}
 
-  return credential
+	return credential, nil
 }
