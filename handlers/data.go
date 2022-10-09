df0b55bd91a2abb45048f6f8fe010cc1673264b7
Marco Martinez <mmmartinez@bluewin.ch>
2022-10-09 17:31:55 +0200

    check status of query

--- handlers/data.go
+++ handlers/data.go
@@ -1,22 +1,27 @@
 package handlers
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/Sevitama/gin-poc/utils/token"
-  "github.com/Sevitama/gin-poc/models"
-  "github.com/gin-gonic/gin"
+	"github.com/Sevitama/gin-poc/models"
+	"github.com/Sevitama/gin-poc/utils/token"
+	"github.com/gin-gonic/gin"
 )
 
 func GetData(c *gin.Context) {
-  user_id, err := token.ExtractTokenID(c)
+	user_id, err := token.ExtractTokenID(c)
 
-  if err != nil {
-    c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
-    return
-  }
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": err.Error()})
+		return
+	}
 
-  credential := models.GetSingleCredential("SELECT * FROM accounts WHERE id = $1", []any{user_id})
+	credential, invalid := models.GetSingleCredential("SELECT * FROM accounts WHERE id = $1", []any{user_id})
 
-  c.HTML(http.StatusOK, "data.html", gin.H{"Id": user_id, "username": credential.Username, "passwordhash": credential.PasswordHash})
+	if invalid != nil {
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"error": "Invalid input"})
+		return
+	}
+
+	c.HTML(http.StatusOK, "data.html", gin.H{"Id": user_id, "username": credential.Username, "passwordhash": credential.PasswordHash})
 }
